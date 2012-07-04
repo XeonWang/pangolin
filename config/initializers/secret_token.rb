80857cefe4c17cbe1007a87cedff82ea2c3a18eb
Xeon <wangzhiqiang1026@gmail.com>
2012-07-04 12:34:53 +0800

    project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pangolin::Application.config.secret_token = '1b4e83a58b64d845e19b22bc01b1b9447ad544ecfc886e58bef31f123e3da787e8101dce80cb2e9094a852156062af601f235258bb7697f009a489276a8fcae7'
