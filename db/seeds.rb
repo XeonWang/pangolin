ccf6a6c71f5b96c83319b0222a947884f6d1b74d
XeonWang <wangzhiqiang1026@gmail.com>
2012-07-08 13:26:21 +0800

    test use the twitter bootstrap, make the page more beautiful

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.delete_all
+Post.create(:user => 1, :content => 'Question: How can i delete an existing model and scaffolding in ruby on rails 3?
+Top answer: rails destroy scaffold MyScaffold')
+Post.create(:user => 2, :content => 'Hi i want to delete all stuff related to rails generate scaffold at once. ... You can use this command and enjoying...... rails destroy scaffold ...')
+Post.create(:user => 3, :content => 'If you then run scaffold again it will warn you of any files that you forgot to delete or any I have forgotten to mention. When I started writing Rails ...')
