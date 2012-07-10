68c9bf742c8b64be840eb2a0847f6015215c5102
Xeon <wangzhiqiang1026@gmail.com>
2012-07-10 11:01:03 +0800

    fixed a error of seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Post.delete_all
 Post.create(:user_id => 1, :content => 'Question: How can i delete an existing model and scaffolding in ruby on rails 3?
-Top answer: rails destroy scaffold MyScaffold', :image_url)
+Top answer: rails destroy scaffold MyScaffold')
 Post.create(:user_id => 2, :content => 'Hi i want to delete all stuff related to rails generate scaffold at once. ... You can use this command and enjoying...... rails destroy scaffold ...')
 Post.create(:user_id => 3, :content => 'If you then run scaffold again it will warn you of any files that you forgot to delete or any I have forgotten to mention. When I started writing Rails ...')
