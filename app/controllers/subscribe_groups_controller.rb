0dfeddffcaeccf9969662aca41331caf68f81ebb
XeonWang <wangzhiqiang1026@gmail.com>
2012-07-20 23:35:59 +0800

    new group with index

--- app/controllers/subscribe_groups_controller.rb
+++ app/controllers/subscribe_groups_controller.rb
@@ -1,7 +1,8 @@
 class SubscribeGroupsController < ApplicationController
 	def create
-		@user = User.find(session[:user_id])
-		@user.subscribe_groups.create(:name => "New Group")
-		render :inline =>"<%= link_to(@user.subscribe_groups.first.name) %>"
+		user = User.find(session[:user_id])
+		index = SubscribeGroup.get_next_name(user.id)
+		@new_group = user.subscribe_groups.create(:name => "New Group"+(index + 1).to_s)
+		render :inline =>"<%= link_to(@new_group.name) %>"
 	end
 end
--- app/models/subscribe_group.rb
+++ app/models/subscribe_group.rb
@@ -1,3 +1,11 @@
 class SubscribeGroup < ActiveRecord::Base
 	# has_and_belongs_to_many :users
+	def self.get_next_name(user_id)
+		last_unnamed = where("user_id = ? and name like 'New Group%'", user_id).order("name desc").first
+		if last_unnamed then
+			return last_unnamed.name.from(9).to_i
+		else
+			return 0
+		end
+	end
 end
