26e9d89c76947092fd512c7e0edef4417b42fa6b
XeonWang <wangzhiqiang1026@gmail.com>
2012-07-23 17:19:12 +0800

    begin to add initial members selection

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    @users = User.all
     @subscribe_groups = session[:user_id] && User.find(session[:user_id]).subscribe_groups
     respond_to do |format|
       format.html # index.html.erb
