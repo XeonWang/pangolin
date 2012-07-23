52879ff85ea6aeaadea1d4169043058a54d905e4
XeonWang <wangzhiqiang1026@gmail.com>
2012-07-23 16:34:05 +0800

    add initial group name input

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -3,6 +3,10 @@ jQuery(document).ready(function(){
 	  $("#add_subscribe_group")
 	    .bind("ajax:success", function(event, data, status, xhr) {
 	      $("#subscrib_groups li:last").after($("<li>"+ data +"</li>"));
+	      $('#myModal').modal('hide');
+	      $("#add_subscribe_group").each (function(){
+		  	this.reset();
+		  });
 	    });
 	});
 });
--- app/controllers/subscribe_groups_controller.rb
+++ app/controllers/subscribe_groups_controller.rb
@@ -1,8 +1,8 @@
 class SubscribeGroupsController < ApplicationController
 	def create
 		user = User.find(session[:user_id])
-		index = SubscribeGroup.get_next_name(user.id)
-		@new_group = user.subscribe_groups.create(:name => "New Group"+(index + 1).to_s)
+		group_name = params[:name]
+		@new_group = user.subscribe_groups.create(:name => group_name)
 		render :inline =>"<%= link_to(@new_group.name) %>"
 	end
 end
