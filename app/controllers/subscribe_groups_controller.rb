726d812160c11de8e22a20dc0330ec59c1527fde
XeonWang <wangzhiqiang1026@gmail.com>
2012-07-24 12:13:37 +0800

    can save member of group info

--- app/controllers/subscribe_groups_controller.rb
+++ app/controllers/subscribe_groups_controller.rb
@@ -2,7 +2,9 @@ class SubscribeGroupsController < ApplicationController
 	def create
 		user = User.find(session[:user_id])
 		group_name = params[:name]
-		@new_group = user.subscribe_groups.create(:name => group_name)
+		@new_group = user.subscribe_groups.build(:name => group_name)
+		@new_group.member_ids = params[:member_users]
+		@new_group.save
 		render :inline =>"<%= link_to(@new_group.name) %>"
 	end
 end
