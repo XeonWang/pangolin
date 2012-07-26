375da064f1e8ff353fe151fb45831b326d5f2578
XeonWang <wangzhiqiang1026@gmail.com>
2012-07-26 16:58:08 +0800

    fix the create new post record when favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
 	def create
 		if session[:user_id] && params[:post_id]
 			user = User.find(session[:user_id])
-			user.favorites.create(:id => params[:post_id])
+			user.favorites << Post.find(params[:post_id])
 		end
 		render :text => "{success}"
 	end
